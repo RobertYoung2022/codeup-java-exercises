e1ba5c6658c7a83bce584c6e83f9904de504a67a
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-02-17 21:15:01 -0600

    completed Contol Flow Exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,145 @@
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Formatter;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("this is i at: " + i);
+//            i++;
+//        }
+//        System.out.println("----------");
+
+//        long j = 2;
+//        do {
+//            System.out.println("this is j at: " + j);
+//            j+=2;
+//        } while (j <= 100);
+//        do {
+//            System.out.println("this is j at: " + j);
+//            j-=5;
+//        } while (j >= -10);
+//        do {
+//            System.out.println("this is j at: " + j);
+//            j*=j;
+//        } while (j < 1000000); // notice: for this volume of number, you have to change type of the variable to correct parameters.
+
+//        for (int j = 100; j >= -10; j-=5 ) {
+//            System.out.println("j is at: " + j);
+//        }
+//        System.out.println("----------> break <--------");
+//        for (long j = 2; j < 1000000; j*=j) {
+//            System.out.println("j is at: " + j);
+//        }
+
+        // ex 2: FIZZBUZZ
+        /*One of the most common interview questions for entry-level programmers is the FizzBuzz test.
+        Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+
+        Write a program that prints the numbers from 1 to 100.
+        For multiples of three: print “Fizz” instead of the number.
+        For the multiples of five: print “Buzz”.
+        For numbers which are multiples of both three and five: print “FizzBuzz”.
+
+         */
+//        int i = 0;
+//        for (i = 0; i <= 100; i++){
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            }
+//            else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            }
+//            else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            }
+//            System.out.println(i);
+//        }
+
+        // ex 3: DISPLAY A TABLE OF POWERS
+        Scanner scanner = new Scanner(System.in);
+//
+        String userChoice;
+        do {
+            System.out.println("Enter an integer for your table of squares: ");
+            int userInput = scanner.nextInt();
+            System.out.printf("your number is: %s.%n", userInput);
+
+            for (int i = 1; i <= userInput; i++) {
+                Formatter format = new Formatter();
+                format.format("%4d %4d %4d", i, i * i, i * i * i);
+                System.out.println(format);
+            }
+
+            System.out.println("Would you like to continue?: [y/N]");
+            userChoice = scanner.next();
+
+        } while (userChoice.equals("y"));
+
+        if (userChoice.equals("n")) {
+            System.out.println("Okay, that's enough for today...");
+        }
+
+        // ex 4: CONVERT GIVEN NUMBER GRADES INTO LETTER GRADES
+
+        String answer;
+        do {
+            System.out.println("Enter a number 0-100: ");
+            int studentScore = scanner.nextInt();
+            System.out.println("number input is: " + studentScore);
+
+            String grade;
+
+            switch (studentScore / 10) {
+                case 10:
+                    grade = "A+!";
+                    break;
+                case 9:
+                    grade = "A!";
+                    break;
+                case 8:
+                    grade = "B!";
+                    break;
+                case 7:
+                    grade = "C!";
+                    break;
+                case 6:
+                    grade = "D!";
+                    break;
+                default:
+                    grade = "F!";
+                    break;
+
+            }
+            System.out.println("Your grade is: " + grade);
+            System.out.println("Would you like to continue?: [y/N]");
+            answer = scanner.next();
+        } while (answer.equals("y"));
+
+        if (answer.equals("n")) {
+            System.out.println("Okay, we'll stop here...");
+        }
+
+    } // <--------- end of main
+} // <--------- end of class
+
+
+
+// ---------->  NOTES ON LOOPS  <----------
+//        for(int i = 0; i <= 10; i++){
+//            System.out.println("i is at: " + i);
+//        }
+
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println("i is: " + i);
+//            i++;
+//        }
+
+//        int i = 0;
+//        do {
+//            System.out.println("i is at: " + i);
+//            i++;
+//        } while (i< 10);
--- src/SyntaxTypesVariables.java
+++ src/SyntaxTypesVariables.java
@@ -4,9 +4,9 @@ public class SyntaxTypesVariables {
     public static void main(String[] args) {
 
         // 1 - 1
-//        double pi = 3.14159;
-//        System.out.println("The value of pi is approximately 3.14.", pi);
-//        System.out.printf("The value of pi is approximately 3.14.", pi);
+        double pi = 3.14159;
+//        System.out.println("The value of pi is approximately 3.14.", pi); // Error received -- no suitable method found for println(java.lang.String,double)
+        System.out.printf("The value of pi is approximately %.2f%n", pi);
 
 
         // 2 - 1
@@ -29,36 +29,66 @@ public class SyntaxTypesVariables {
 
 //        Scanner whatWord = new Scanner(System.in);
 //
-//        System.out.print("What is your first word?");
+//        // prompt user to enter three words
+//        System.out.print("Enter 3 words to describe you: ");
+
+//        // putting each word into a variable
 //        String word1 = whatWord.next();
-//
-//        System.out.print("What is your second word?");
 //        String word2 = whatWord.next();
-//
-//        System.out.print("What is your third word?");
 //        String word3 = whatWord.next();
 //
-//
+//        // printing out each variable
 //        System.out.println(word1);
 //        System.out.println(word2);
 //        System.out.println(word3);
 
 
         // 2 - 3
-        Scanner favFood = new Scanner(System.in);
-
-        System.out.print("Describe your favorite food in one sentence?");
-        String yourFavFood = favFood.next(); // Only printed the first word of the sentence
-        System.out.println(yourFavFood);
-
-        // 2 - 4
-        Scanner favHobby = new Scanner(System.in);
-
-        System.out.print("In one sentence, describe your favorite hobby?");
-        String yourFavHobby = favHobby.nextLine(); // Printed the whole sentence
-        System.out.println(yourFavHobby);
+//        Scanner favFood = new Scanner(System.in);
+//
+//        System.out.print("Describe your favorite food in one sentence?");
+//        String yourFavFood = favFood.next(); // Only printed the first word of the sentence
+//        System.out.println(yourFavFood);
+//
+//      // 2 - 4
+//        Scanner favHobby = new Scanner(System.in);
+//
+//        System.out.print("In one sentence, describe your favorite hobby?");
+//        String yourFavHobby = favHobby.nextLine(); // Printed the whole sentence
+//        System.out.println(yourFavHobby);
 
         // 3 - 1
+            // MY WORK
+//        Scanner roomArea = new Scanner(System.in);
+//
+//        System.out.print("Please enter the length of your room:");
+//        String length = roomArea.nextLine();
+//        System.out.print("Please enter the width of your room:");
+//        String width = roomArea.nextLine();
+
+//        int widthNum = Integer.parseInt(width);
+//        int lengthNum = Integer.parseInt(length);
+//
+//        int area = width * length;
+//        int perimeter = 2 * (width + length);
+//
+//        System.out.format(" The Area of a Rectangle = %.2f\n", area);
+//        System.out.format(" The Perimeter of a Rectangle = %.2f\n", perimeter);
+
+
+//        // EXAMPLE WITH NEXT DOUBLE
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter the length of the room: ");
+//        double length = scanner.nextDouble();
+//
+//        System.out.println("Enter the width of the room: ");
+//        double width = scanner.nextDouble();
+//
+//        // area = length * width;
+//        double area = length * width;
+//        System.out.println("Area of the room is: " + area);
+
 
 
 
@@ -92,5 +122,12 @@ public class SyntaxTypesVariables {
 //        System.out.printf("the area of your room is: %.2f%s%n", (userLengthInt * userWidthInt), " square feet.");
 //        System.out.printf("the perimeter of your room is: %.2f%s%n", (userLengthInt * 2) + (userWidthInt * 2), " feet.");
 //        System.out.printf("the area of your room is: %.2f%s%n", (userLengthInt * userWidthInt * userHeightInt), " cubic feet.");
+
+
+//        int myNum = 8;
+//        boolean isIt = myNum < 88;
+//
+//        System.out.println("My number is " + isIt);
+
     }
 }
