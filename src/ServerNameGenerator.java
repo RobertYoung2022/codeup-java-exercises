c23a021ae846e0cecfda69ef449b576df33b627d
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-01 15:27:19 -0600

    completed server name generator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 public class ServerNameGenerator {
     // two arrays 10 adjectives / 10 nouns
@@ -9,22 +11,33 @@ public class ServerNameGenerator {
 
     // method to return random element from arrays string (Adjective and Noun)
 
+
+    public static String randomWord(String[] pickedWord) {
+        int random = new Random().nextInt(pickedWord.length);
+        return pickedWord[random];
+    }
+
+
     public static void main(String[] args) {
 
         // iterate through adjectives object
-        for (int i = 0; i < adjective.length; i += 1) {
-            System.out.print("Adjective object: ");
-            System.out.println(adjective[i]);
-        }
+//        for (int i = 0; i < adjective.length; i += 1) {
+//            System.out.print("Adjective object: ");
+//            System.out.println(adjective[i]);
+//        }
 
         System.out.println();
 
         // iterate through nouns object
-        for (int i = 0; i < noun.length; i += 1) {
-            System.out.print("Noun object: ");
-            System.out.println(noun[i]);
-        }
+//        for (int i = 0; i < noun.length; i += 1) {
+//            System.out.print("Noun object: ");
+//            System.out.println(noun[i]);
+//        }
+        System.out.print("Noun object: ");
+        System.out.println(randomWord(noun));
+//        System.out.println();
+        System.out.print("Adjective object: ");
+        System.out.println(randomWord(adjective));
     }
-
 
 }
