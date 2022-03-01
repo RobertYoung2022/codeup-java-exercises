e4bcd575ff3c8bb1b74937766e42761e1a86f49d
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-01 14:02:19 -0600

    practicing

---
+++ src/ArraysExercises.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+    public static void main(String[] args) {
+        // What happens when you run the following code? Why is Arrays.toString necessary?
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers); // [I@6bdf28bb
+
+
+        // 1 .
+
+        String[] Person;
+        Person = new String[3];
+
+        Person[0] = "Robert";
+        Person[1] = "Gerald";
+        Person[2] = "Remee";
+
+        public static String addPerson() {
+
+        }
+
+    }
+}
