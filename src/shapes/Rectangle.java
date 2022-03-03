9c87fecf04bd616d271a14e880a03b1cc194c8f8
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-02 18:07:19 -0600

    created get area/perimeter files

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,15 @@
 package shapes;
 
 public class Rectangle {
+
+    // protected properties for both length and width.
+    protected int rectLength;
+    protected int rectWidth;
+
+
+    // define a constructor that accepts two numbers for length and width, and sets those properties
+    public Rectangle(int rectLength, int rectWidth) {
+        this.rectLength = rectLength;
+        this.rectWidth = rectWidth;
+    }
 }
