f5dfd1b5630f8db49700b9ab93cb9c6fe121dd03
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-08 10:55:23 -0600

    complete exception lecture

--- src/ExceptionsTest.java
+++ src/ExceptionsTest.java
@@ -1,6 +1,6 @@
 public class ExceptionsTest {
     public static void main(String[] args) throws Exception {
-        System.out.println("Good Morning, Sirius");
+//        System.out.println("Good Morning, Sirius");
 //        throw new Exception("Woops - Exception encountered");
 
 
@@ -13,18 +13,24 @@ public class ExceptionsTest {
 
         try {
             // after new - insert the type of exception you want to print the exception for
-            throw new NullPointerException("Something broke while running the application!");
-        } catch (NullPointerException exceptionObject) { // path A - if I change the new <type>Exception
+            throw new Exception("Something broke while running the application!");
+        } catch (NullPointerException exceptionObject) {
+            // path A - if I change the new <type>Exception
+            // completely specific
             System.out.println("This is a null pointer exception");
-            System.out.println("Here is an example of .getMessage()" + exceptionObject.getMessage());
+            System.out.println("Here is an example of .getMessage() " + exceptionObject.getMessage());
             exceptionObject.printStackTrace();
-        } catch (RuntimeException exceptionObject) { // path B - if I change the new <type>Exception
+        } catch (RuntimeException exceptionObject) {
+            // path B - if I change the new <type>Exception
+            //  more specific
             System.out.println("This is a runtime exception");
-            System.out.println("Here is an example of .getMessage()" + exceptionObject.getMessage());
+            System.out.println("Here is an example of .getMessage() " + exceptionObject.getMessage());
             exceptionObject.printStackTrace();
-        } catch (Exception exceptionObject) { // default
+        } catch (Exception exceptionObject) {
+            // path C - default
+            // least specific
             System.out.println("this is a generic, top level exception");
-            System.out.println("Here is an example of .getMessage()" + exceptionObject.getMessage());
+            System.out.println("Here is an example of .getMessage() " + exceptionObject.getMessage());
             exceptionObject.printStackTrace();
         }
 //        System.out.println("Testing the location of this sout"); // printed above the exception object
