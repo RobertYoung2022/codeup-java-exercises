7c6f98512f5b5c557d935b9e0198b962e1e719d7
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-11 13:51:29 -0600

    created interfaces/abstract exercise file

--- src/Person.java
+++ src/Person.java
@@ -36,5 +36,18 @@ public class Person {
         Employee emp = new Employee("Peter Gibbons", "IT");
         System.out.println(emp.getName() + " works in " + emp.getDepartment());
 
+
+        String[] names = {"Rick", "Stacey", "Brad", "Becca"};
+        String x;
+        try {
+            x = names[100];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught!");
+            x = "";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("This will always run.");
+        }
     }
 }
