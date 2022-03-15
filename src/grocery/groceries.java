9c61b2fdc830b1696d513336e490cbaa86b7a5e7
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-03-14 19:55:09 -0500

    grocery list--do while loop

--- src/grocery/groceries.java
+++ src/grocery/groceries.java
@@ -1,10 +1,33 @@
 package grocery;
 
+import java.util.Scanner;
+
 public class groceries {
     public String newItem;
 
 
-    public static void main(String[] args) {
+    // A user should be prompted if they would like to create a grocery list.
 
+    public static void main(String[] args) {
+        Scanner searchList = new Scanner(System.in);
+
+        boolean loop = true;
+
+        System.out.println("* * * * * * * * * * *");
+        System.out.println("Good Morning Shopper!");
+        System.out.println("* * * * * * * * * * *");
+
+        while(loop) {
+            System.out.println("Would you like to create a Grocery List?");
+            System.out.println("Search Again? [Y/N]");
+            String shopperReply = searchList.nextLine();
+            if (shopperReply.equalsIgnoreCase("Y")) {
+                loop = true;
+            } else {
+                System.out.println("Thank you... Have a great day!");
+                loop = false;
+            }
+            loop = false;
+        }
     }
 }
